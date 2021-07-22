a857b515ab910d257eea67e22672d718b28dae5e
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-07-22 20:21:23 +0530

    Add files via upload

--- AliciaMusic/modules/play.py
+++ AliciaMusic/modules/play.py
@@ -551,7 +551,7 @@ async def play(_, message: Message):
         )
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/0e0509861103d84810678.jpg
+        thumb_name = "https://telegra.ph/file/0e0509861103d84810678.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
